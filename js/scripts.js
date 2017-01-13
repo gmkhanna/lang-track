15448ba8b05b8a554fc727efd3b2e6c98dc6565a
Grinil Khanna <gmkhanna@gmail.com>
2017-01-13 11:28:29 -0800

    modify images in result area - correct size. 200px

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,18 @@ var add = function(q1, q2) {
   return q1 + q2;
 };
 
+// FRONT-END; INTERFACE LOGIC ///
 
 $(document).ready(function() {
   $("#survey form").submit(function(event) {
-    event.preventDefault();
     $(".result").hide();
 
     var javaQ1 = parseInt($("#javaQ1").val());
     var javaQ2 = parseInt($("#javaQ2").val());
     var resultJava = add(javaQ1, javaQ2);
-    alert(resultJava);
+    $(".result-area").show();
+    $("#java-result").show();
+
+    event.preventDefault();
   });
 });
