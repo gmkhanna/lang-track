1fe8bef1174675bd5e829895f5cf33d063a9e7e4
Grinil Khanna <gmkhanna@gmail.com>
2017-01-13 14:18:15 -0800

    add java language else if conditions and pic and results. All work. Need to add the other 3 languages.

--- js/scripts.js
+++ js/scripts.js
@@ -17,31 +17,34 @@ $(document).ready(function() {
     var totalJava = add(javaQ1, javaQ2);
 
     $(".result").show();
-    $("#java-result").show();
 
-    $("#ruby-result").show();
-    $("#cs-result").show();
-$("#php-result").show();
+      if (totalJava === 0) {
+      $("#java-result").fadeIn();
+      $("#result00").fadeIn(10000);
+    } else if (totalJava === 05) {
+      $("#java-result").fadeIn();
+      $("#result05").fadeIn(7000);
+    } else if (totalJava === 10) {
+      $("#java-result").fadeIn();
+      $("#result10").fadeIn(5000);
+    } else if (totalJava === 15) {
+      $("#java-result").fadeIn();
+      $("#result15").fadeIn(3000);
+    } else {
+      $("#java-result").fadeIn();
+      $("#result20").fadeIn();
+    }
 
-    // if (totalJava === 20) {
-    //   $("#java-result").fadeIn(10000);
-    //   $("#result20").show();
-    // } else {
-    // }
-
-  //   if (resultJava === 0) {
-  //   $("#java-result").show();
-  //   $("#result00").show();
-  // } else if (resultJava === 05) {
-  //   $("#java-result").fadeIn();
-  //   $("#result05").fadeIn(2000);
-  // } else if (resultJava === 10) {
-  //   $("#java-result").fadeIn();
-  //   $("#result10").fadeIn(4000);
-  // } else if (resultJava === 15) {
-  //   $("#java-result").fadeIn();
-  //   $("#result15").fadeIn(6000);
-
+  // $("#java-result").show();
+  // $("#ruby-result").show();
+  // $("#cs-result").show();
+  // $("#php-result").show();
+  //
+  // $("#result20").show();
+  // $("#result15").show();
+  // $("#result10").show();
+  // $("#result05").show();
+  // $("#result00").show();
     event.preventDefault();
   });
 });
