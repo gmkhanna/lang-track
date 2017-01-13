e75301bcc189b5ed9027c0423c964f0fdd58e208
Grinil Khanna <gmkhanna@gmail.com>
2017-01-13 11:51:26 -0800

    Add code in process for java result fade in and results. Still need to test.

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,26 @@ $(document).ready(function() {
     var javaQ1 = parseInt($("#javaQ1").val());
     var javaQ2 = parseInt($("#javaQ2").val());
     var resultJava = add(javaQ1, javaQ2);
+
     $(".result-area").show();
     $("#java-result").show();
+
+    if (resultJava = 20) {
+    $("#java-result").fadeIn();
+    $("#result20").fadeIn();
+  } else if (resultJava = 15) {
+    $("#java-result").fadeIn();
+    $("#result15").fadeIn(2000);
+  } else if (resultJava = 10) {
+    $("#java-result").fadeIn();
+    $("#result10").fadeIn(4000);
+  } else if (resultJava = 05) {
+    $("#java-result").fadeIn();
+    $("#result05").fadeIn(6000);
+  } else if (resultJava = 0) {
+    $("#java-result").fadeIn();
+    $("#result00").fadeIn();
+  }
 
     event.preventDefault();
   });
