c3987b10648ab6b60b42ac44a8bbc7bcaebc709f
Grinil Khanna <gmkhanna@gmail.com>
2017-01-13 10:33:02 -0800

    Test to java q1 and q2 add through alert - it works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
+// BACK-END LOGIC //
+
+var add = function(q1, q2) {
+  return q1 + q2;
+};
+
+
 $(document).ready(function() {
   $("#survey form").submit(function(event) {
     event.preventDefault();
     $(".result").hide();
 
+    var javaQ1 = parseInt($("#javaQ1").val());
+    var javaQ2 = parseInt($("#javaQ2").val());
+    var resultJava = add(javaQ1, javaQ2);
+    alert(resultJava);
   });
 });
