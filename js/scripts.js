252aafdb3bd6f9a5c9771ce6586764e965264615
Grinil Khanna <gmkhanna@gmail.com>
2017-01-13 15:48:02 -0800

    Add div changes to images and phrase sections. Undersatanding more of nested divs and hiding/showing.

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,8 @@ var add = function(q1, q2) {
 
 $(document).ready(function() {
   $("#survey form").submit(function(event) {
+    $(".result").show();
+    $(".resultImg").show();
 
     var javaQ1 = parseInt($("#javaQ1").val());
     var javaQ2 = parseInt($("#javaQ2").val());
@@ -17,7 +19,20 @@ $(document).ready(function() {
     var rubyQ2 = parseInt($("#rubyQ2").val());
     var totalRuby = add(rubyQ1, rubyQ2);
 
+    var csQ1 = parseInt($("#csQ1").val());
+    var csQ2 = parseInt($("#csQ2").val());
+    var totalCs = add(csQ1, csQ2);
 
+    var phpQ1 = parseInt($("#phpQ1").val());
+    var phpQ2 = parseInt($("#phpQ2").val());
+    var totalPhp = add(phpQ1, phpQ2);
+
+
+    
+    $("#javaImg").hide();
+    $("#rubyImg").hide();
+    $("#csImg").hide();
+    $("#phpImg").hide();
 
 
 
