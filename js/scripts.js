c90d1229f2ec99bd6cfa28413068cbb141841d05
Grinil Khanna <gmkhanna@gmail.com>
2017-01-13 14:04:30 -0800

    fix divs under form survey section. Result class needs to enclose entire result, and indiv divs need to be done for each text and img result. ID to each unique phrase/img, yet homogenous class to each div (2 total); one class for image, one class for phrase. These classes go in the hide function in jQuery, and the ids go in the show function on else if detemrniations since theses IDs and classes are on the same level.

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,26 @@ var add = function(q1, q2) {
 
 $(document).ready(function() {
   $("#survey form").submit(function(event) {
+
     $(".resultImg").hide();
+    $(".resultPhrase").hide();
 
     var javaQ1 = parseInt($("#javaQ1").val());
     var javaQ2 = parseInt($("#javaQ2").val());
     var totalJava = add(javaQ1, javaQ2);
 
-    if (totalJava === 20) {
-      $("#java-result").fadeIn(6000);
-      $("#result20").show();
-    } else {
-    }
+    $(".result").show();
+    $("#java-result").show();
 
-    $(".result-text").fadeIn(10000);
+    $("#ruby-result").show();
+    $("#cs-result").show();
+$("#php-result").show();
+
+    // if (totalJava === 20) {
+    //   $("#java-result").fadeIn(10000);
+    //   $("#result20").show();
+    // } else {
+    // }
 
   //   if (resultJava === 0) {
   //   $("#java-result").show();
