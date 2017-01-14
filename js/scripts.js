5c2853bb963aa22d76b87862a760cc15aa2174de
Grinil Khanna <gmkhanna@gmail.com>
2017-01-13 16:43:04 -0800

    Give a fully working site.

--- js/scripts.js
+++ js/scripts.js
@@ -65,6 +65,124 @@ $(document).ready(function() {
           $("#javaPhrase00").show();
       }
 
+// here is ruby
+
+if (totalRuby === 20) {
+  $("#rubyImg").show();
+    $("#rubyPhrase20").show();
+    $("#rubyPhrase15").hide();
+    $("#rubyPhrase10").hide();
+    $("#rubyPhrase05").hide();
+    $("#rubyPhrase00").hide();
+} else if (totalRuby === 15) {
+  $("#rubyImg").show();
+    $("#rubyPhrase20").hide();
+    $("#rubyPhrase15").show();
+    $("#rubyPhrase10").hide();
+    $("#rubyPhrase05").hide();
+    $("#rubyPhrase00").hide();
+} else if (totalRuby === 10) {
+  $("#rubyImg").show();
+    $("#rubyPhrase20").hide();
+    $("#rubyPhrase15").hide();
+    $("#rubyPhrase10").show();
+    $("#rubyPhrase05").hide();
+    $("#rubyPhrase00").hide();
+  }  else if (totalRuby === 05) {
+    $("#rubyImg").show();
+    $("#rubyPhrase20").hide();
+    $("#rubyPhrase15").hide();
+    $("#rubyPhrase10").hide();
+    $("#rubyPhrase05").show();
+    $("#rubyPhrase00").hide();
+  } else {
+    $("#rubyImg").show();
+      $("#rubyPhrase20").hide();
+      $("#rubyPhrase15").hide();
+      $("#rubyPhrase10").hide();
+      $("#rubyPhrase05").hide();
+      $("#rubyPhrase00").show();
+  }
+
+// here is PHP
+
+if (totalPhp === 20) {
+  $("#phpImg").show();
+    $("#phpPhrase20").show();
+    $("#phpPhrase15").hide();
+    $("#phpPhrase10").hide();
+    $("#phpPhrase05").hide();
+    $("#phpPhrase00").hide();
+} else if (totalPhp === 15) {
+  $("#phpImg").show();
+    $("#phpPhrase20").hide();
+    $("#phpPhrase15").show();
+    $("#phpPhrase10").hide();
+    $("#phpPhrase05").hide();
+    $("#phpPhrase00").hide();
+} else if (totalPhp === 10) {
+  $("#phpImg").show();
+    $("#phpPhrase20").hide();
+    $("#phpPhrase15").hide();
+    $("#phpPhrase10").show();
+    $("#phpPhrase05").hide();
+    $("#phpPhrase00").hide();
+  }  else if (totalPhp === 05) {
+    $("#phpImg").show();
+    $("#phpPhrase20").hide();
+    $("#phpPhrase15").hide();
+    $("#phpPhrase10").hide();
+    $("#phpPhrase05").show();
+    $("#phpPhrase00").hide();
+  } else {
+    $("#phpImg").show();
+      $("#phpPhrase20").hide();
+      $("#phpPhrase15").hide();
+      $("#phpPhrase10").hide();
+      $("#phpPhrase05").hide();
+      $("#phpPhrase00").show();
+  }
+
+// Here is CS
+
+if (totalCs === 20) {
+  $("#csImg").show();
+    $("#csPhrase20").show();
+    $("#csPhrase15").hide();
+    $("#csPhrase10").hide();
+    $("#csPhrase05").hide();
+    $("#csPhrase00").hide();
+} else if (totalCs === 15) {
+  $("#csImg").show();
+    $("#csPhrase20").hide();
+    $("#csPhrase15").show();
+    $("#csPhrase10").hide();
+    $("#csPhrase05").hide();
+    $("#csPhrase00").hide();
+} else if (totalCs === 10) {
+  $("#csImg").show();
+    $("#csPhrase20").hide();
+    $("#csPhrase15").hide();
+    $("#csPhrase10").show();
+    $("#csPhrase05").hide();
+    $("#csPhrase00").hide();
+  }  else if (totalCs === 05) {
+    $("#csImg").show();
+    $("#csPhrase20").hide();
+    $("#csPhrase15").hide();
+    $("#csPhrase10").hide();
+    $("#csPhrase05").show();
+    $("#csPhrase00").hide();
+  } else {
+    $("#csImg").show();
+      $("#csPhrase20").hide();
+      $("#csPhrase15").hide();
+      $("#csPhrase10").hide();
+      $("#csPhrase05").hide();
+      $("#csPhrase00").show();
+  }
+
+
     event.preventDefault();
   });
 });
