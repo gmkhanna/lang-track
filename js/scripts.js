7586541b5fc9affe39e56261653c689e0be3fd0a
Grinil Khanna <gmkhanna@gmail.com>
2017-01-16 23:24:34 -0800

    Add an attempt for looping and arrays but appears to be unsuccessful - commented out.

--- js/scripts.js
+++ js/scripts.js
@@ -17,24 +17,24 @@ $(document).ready(function() {
 
     var softwareName = ["javaQ1", "javaQ2", "rubyQ1", "csQ1", "csQ2", "phpQ1", "phpQ2"];
 
-    softwareName.forEach(function(software) {
-        var questionPoint = parseInt($("#" + softwareName).val();
-    });
-
-    // var javaQ1 = parseInt($("#javaQ1").val());
-    // var javaQ2 = parseInt($("#javaQ2").val());
+    // softwareName.forEach(function(software) {
+    //     var questionPoint = parseInt($("#" + softwareName).val();
+    // });
+    //
+    var javaQ1 = parseInt($("#javaQ1").val());
+    var javaQ2 = parseInt($("#javaQ2").val());
     var totalJava = add(javaQ1, javaQ2);
 
-    // var rubyQ1 = parseInt($("#rubyQ1").val());
-    // var rubyQ2 = parseInt($("#rubyQ2").val());
+    var rubyQ1 = parseInt($("#rubyQ1").val());
+    var rubyQ2 = parseInt($("#rubyQ2").val());
     var totalRuby = add(rubyQ1, rubyQ2);
 
-    // var csQ1 = parseInt($("#csQ1").val());
-    // var csQ2 = parseInt($("#csQ2").val());
+    var csQ1 = parseInt($("#csQ1").val());
+    var csQ2 = parseInt($("#csQ2").val());
     var totalCs = add(csQ1, csQ2);
 
-    // var phpQ1 = parseInt($("#phpQ1").val());
-    // var phpQ2 = parseInt($("#phpQ2").val());
+    var phpQ1 = parseInt($("#phpQ1").val());
+    var phpQ2 = parseInt($("#phpQ2").val());
     var totalPhp = add(phpQ1, phpQ2);
 
 
